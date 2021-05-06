6a4045a270ccfc0cca59ce3124220eb8abe3cdd1
Johannes Nau <johannes.nau@tu-ilmenau.de>
2021-05-06 12:13:12 +0200

    implements multi select for the instances page

--- src/pages/instances.ts
+++ src/pages/instances.ts
@@ -1,31 +1,64 @@
-import { html, customElement, css } from "lit-element";
+import { ConfirmDialog } from "@src/components/dialogs";
+import { InstanceList } from "@src/components/instance";
+import { delInstance } from "@src/store/instance";
+import { store } from "@src/store/store";
+import { html, customElement, css, property, query } from "lit-element";
 import { BasePage } from "./base";
 
 @customElement("koi-instances")
 export class KoiInstances extends BasePage {
-  isSubPage = false;
-  head() {
-    return html`Instances`;
-  }
+    isSubPage = false;
 
-  actions() {
-    return html``;
-  }
+    @property()
+    private selectionMode: boolean = false;
 
-  content() {
-    return html` <paper-card>
-      <instance-list .scrollTarget=${this.scrollTarget}></instance-list>
-    </paper-card>`;
-  }
+    @query("#deleteDialog")
+    deleteDialog: ConfirmDialog;
 
-  static get styles() {
-    return super.styles.concat(
-      css`
+    @query("instance-list")
+    list: InstanceList;
+
+    head() {
+        return html`Instances`;
+    }
+
+    actions() {
+        return this.selectionMode ? html`
+          <mwc-icon-button
+            icon="delete"
+            slot="actionItems"
+            @click=${() => this.deleteDialog.show()}
+          ></mwc-icon-button>`: html``;
+    }
+
+    content() {
+        return html` <paper-card>
+      <instance-list
+        .scrollTarget=${this.scrollTarget}
+        @selectionMode=${(event) => {
+                this.selectionMode = event.detail.selectionModeActive;
+            }}
+      ></instance-list>
+    </paper-card>
+    <confirm-dialog id="deleteDialog" @confirm=${this.delete}>
+    Are you sure you want to delete the selected instances?
+  </confirm-dialog>`;
+    }
+
+    delete() {
+        for (const listItem of this.list.selection) {
+            store.dispatch(delInstance({ id: listItem.instance }));
+        }
+    }
+
+    static get styles() {
+        return super.styles.concat(
+            css`
         paper-card {
           max-width: var(--std-width);
           width: 100%;
         }
       `
-    );
-  }
+        );
+    }
 }
